3e1cb28098830c8ac765773b25f31daffa9a0b6d
Bonolom <bonolom@vatit.com>
2022-04-26 20:06:18 +0200

    init commit

---
+++ src/test/kotlin/com/openbloq/projectmorpheus/ProjectMorpheusApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.openbloq.projectmorpheus
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjectMorpheusApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
